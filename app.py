b9a71f4a6ddb7dbdc79c29c634340d048f269f91
adrianoraraujo <31390563+adrianoraraujo@users.noreply.github.com>
2020-08-05 19:18:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def main():
    return render_template('home.html')   
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=sys.argv[1]) # Executa a aplicação
-   #app.run(debug=True)
+   #app.run(host='0.0.0.0', port=sys.argv[1]) # Executa a aplicação
+   app.run(debug=True)
